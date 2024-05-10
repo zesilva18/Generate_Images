3d0491fa35b8bc3c995d30ff4307ab8c3df72272
Diogo Fontes <84334953+DiogoFontes01@users.noreply.github.com>
2024-05-10 20:42:16 +0100

    Add files via upload

--- data_augmentation.py
+++ data_augmentation.py
@@ -25,11 +25,17 @@ save_dir = 'augmented_images'
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
+# Limite de imagens a serem geradas por subpasta
+images_per_subfolder_limit = 50
+
 # Processar apenas as subpastas especificadas
 for subfolder in subfolders_augment:
     subfolder_path = os.path.join(train_dir, subfolder)
     if os.path.isdir(subfolder_path):
+        image_count = 0  # Contador para limitar o número de imagens geradas
         for image_file in os.listdir(subfolder_path):
+            if image_count >= images_per_subfolder_limit:
+                break  # Se atingir o limite, interrompe o loop para essa subpasta
             image_path = os.path.join(subfolder_path, image_file)
             image = load_img(image_path, target_size=(32, 32))
             image = img_to_array(image)
@@ -38,3 +44,4 @@ for subfolder in subfolders_augment:
             img = next(image_generator)[0]  # Obter a primeira imagem do batch
             filename = os.path.join(save_dir, f'augmented_{subfolder}_{image_file}')
             tf.keras.preprocessing.image.save_img(filename, img)
+            image_count += 1  # Incrementar contador de imagens salvas
